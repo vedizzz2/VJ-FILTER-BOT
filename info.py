6ca28b728afdbb6eaa22fb1f44f6a43d0fbe682c
vedizzz2 <son491413@gmail.com>
2025-04-14 17:39:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://lucypro.koyeb.app")
+URL = environ.get("URL", "https://www.tgxdl4.workers.dev")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
